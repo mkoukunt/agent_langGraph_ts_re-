45312a75c52f69fbf39837a78e184d9a636a2da9
Madhu Goud <mgoud@crexendo.com>
2026-04-23 16:13:54 -0700

    changed from axios to fetch

--- src/api/apiSvc.ts
+++ src/api/apiSvc.ts
@@ -1,7 +1,8 @@
 import { apiClient } from './apiClient';
 
 export const agentApi = async (b: { qn: string, apiHost: string, accessToken: string }): Promise<string> => {  
-  const { data } = await apiClient.post<{}>('/agent', b); // Adjust the endpoint as needed  
-  return {data}.data; // Adjust based on your API response structure
+  const data  = await fetch('/agent', {method:'POST',headers: { 'Content-Type': 'application/json' },body:JSON.stringify(b)}); // Adjust the endpoint as needed  
+  
+  return data.json(); // Adjust based on your API response structure
 };
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,16 +11,30 @@ export function NSbutton({
   setState: React.Dispatch<React.SetStateAction<AgentStore>>;
 }) {
   const handleClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
-
+     setState((prevState) => ({
+      ...prevState,
+      ans: "", // Replace with actual API response when ready,
+    }));
+    const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
     console.log("Question:", st.qn);
 
-    let data=await agentApi({qn: st.qn,apiHost: st.apiHost, accessToken: st.accessToken}); // Replace with actual API call when ready
-
-console.log("API Response:", data);
-    setState((prevState) => ({
+    let data = await agentApi({
+      qn: st.qn,
+      apiHost: st.apiHost,
+      accessToken: st.accessToken,
+    }); // Replace with actual API call when ready
+   // const words = JSON.stringify(data).split(/([+-])/);
+   // let res="";
+   // for (let word of words) {
+    //  res =res+" "+word;
+      setState((prevState) => ({
       ...prevState,
-      ans: JSON.stringify(data), // Replace with actual API response when ready,
+      ans:JSON.stringify(data), // Replace with actual API response when ready,
     }));
+   //   await sleep(5); // Pause for 150ms between words
+   // }
+
+   
   };
   return (
     <div className="item">
