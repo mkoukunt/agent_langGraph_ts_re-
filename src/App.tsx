494caaa36cb61a73e0276f11f4bd3ed61fd6fdce
Madhu Goud <mgoud@crexendo.com>
2026-05-08 16:51:58 -0700

    Route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import { Agent } from "./components/Agent";
 
 function App() {
-  return <Agent title="Agent" />;
+  return ( 
+    <BrowserRouter>
+      {/* Navigation */}
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Agent title="Agent" />} />       
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
